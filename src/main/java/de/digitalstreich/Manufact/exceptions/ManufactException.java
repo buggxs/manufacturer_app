8911efb1a3a98157a2c1ed7357b43a84a4d5f91a
Salzzy <salzi_andre@web.de>
2021-05-09 20:55:20 +0200

    ManufactException implemented

---
+++ src/main/java/de/digitalstreich/Manufact/exceptions/ManufactException.java
@@ -0,0 +1,7 @@
+package de.digitalstreich.Manufact.exceptions;
+
+public class ManufactException extends RuntimeException {
+    public ManufactException(String error) {
+        super(error);
+    }
+}
